c32d09f977650d788ef74559b0e71901225112f1
Segrel Koskentausta <segrealm@gmail.com>
2024-09-11 21:04:27 +0300

    GRU optimization

--- projects/Dynamics/Compressor/LA2AMakeUp.h
+++ projects/Dynamics/Compressor/LA2AMakeUp.h
@@ -15,18 +15,14 @@ public:
         memset(weight_ih_r, 0, sizeof(weight_ih_r));
         memset(weight_ih_z, 0, sizeof(weight_ih_z));
         memset(weight_ih_n, 0, sizeof(weight_ih_n));
-
-        memset(bias_ih_r, 0, sizeof(bias_ih_r));
-        memset(bias_ih_z, 0, sizeof(bias_ih_z));
         memset(bias_ih_n, 0, sizeof(bias_ih_n));
 
         memset(weight_hh_r, 0, sizeof(weight_hh_r));
         memset(weight_hh_z, 0, sizeof(weight_hh_z));
         memset(weight_hh_n, 0, sizeof(weight_hh_n));
-
-        memset(bias_hh_r, 0, sizeof(bias_hh_r));
-        memset(bias_hh_z, 0, sizeof(bias_hh_z));
         memset(bias_hh_n, 0, sizeof(bias_hh_n));
+        memset(bias_r, 0, sizeof(bias_r));
+        memset(bias_z, 0, sizeof(bias_z));
 
         memset(weight_output, 0, sizeof(weight_output));
         memset(bias_output, 0, sizeof(bias_output));
@@ -41,92 +37,44 @@ public:
 
     void process(float * const * output, const float * const * input, unsigned int numSamples)
     {
-        float r_gate[HIDDEN_SIZE];
-        float z_gate[HIDDEN_SIZE];
-        float n_gate[HIDDEN_SIZE];
-        float n_hidden[HIDDEN_SIZE];
+        // This is mostly from Valtteri Kallinen's code in the course Slack.
+        // No fast math tricks to keep the code simple. Still plenty fast on a modern laptop computer.
+        for (size_t n = 0; n < numSamples; ++n) {
+            for (size_t i = 0; i < HIDDEN_SIZE; ++i) {
+                float r_gate = bias_r[i];
+                float z_gate = bias_z[i];
+                float n_gate = bias_ih_n[i];
 
-        for (unsigned int n = 0; n < numSamples; ++n)
-        {
-            // Compute r gate
-            for (unsigned int i = 0; i < HIDDEN_SIZE; ++i)
-            {
-                // Initialise with bias_ih
-                r_gate[i] = bias_ih_r[i];
-                // Implement matrix-vector multiply with weight_ih and the input signal
-                for (unsigned int j = 0; j < INPUT_SIZE; ++j)
-                {
-                    r_gate[i] += weight_ih_r[i][j] * input[n][j];
+                for (size_t j = 0; j < INPUT_SIZE; ++j) {
+                    r_gate = std::fma(weight_ih_r[i][j], input[n][j], r_gate);
+                    z_gate = std::fma(weight_ih_z[i][j], input[n][j], z_gate);
+                    n_gate = std::fma(weight_ih_n[i][j], input[n][j], n_gate);
                 }
-                // Add bias_hh
-                r_gate[i] += bias_hh_r[i];
-                // Implement matrix-vector multiply with weight_hh and the state signal
-                for (unsigned int j = 0; j < HIDDEN_SIZE; ++j)
-                {
-                    r_gate[i] += weight_hh_r[i][j] * state[j];
+
+                float n_hidden = bias_hh_n[i];
+                for (size_t j = 0; j < HIDDEN_SIZE; ++j) {
+                    r_gate = std::fma(weight_hh_r[i][j], old_state[j], r_gate);
+                    z_gate = std::fma(weight_hh_z[i][j], old_state[j], z_gate);
+                    n_hidden = std::fma(weight_hh_n[i][j], old_state[j], n_hidden);
                 }
-                // Apply sigmoid activation function
-                r_gate[i] = sigmoid(r_gate[i]);
+
+                r_gate = sigmoid(r_gate);
+                z_gate = sigmoid(z_gate);
+
+                n_gate = std::fma(n_hidden, r_gate, n_gate);
+                n_gate = std::tanhf(n_gate);
+
+                new_state[i] = std::fma(z_gate, (old_state[i] - n_gate), n_gate);
             }
 
-            // Compute z gate
-            for (unsigned int i = 0; i < HIDDEN_SIZE; ++i)
-            {
-                // Initialise with bias_ih
-                z_gate[i] = bias_ih_z[i];
-                // Implement matrix-vector multiply with weight_ih and the input signal
-                for (unsigned int j = 0; j < INPUT_SIZE; ++j)
-                {
-                    z_gate[i] += weight_ih_z[i][j] * input[n][j];
-                }
-                // Add bias_hh
-                z_gate[i] += bias_hh_z[i];
-                // Implement matrix-vector multiply with weight_hh and the state signal
-                for (unsigned int j = 0; j < HIDDEN_SIZE; ++j)
-                {
-                    z_gate[i] += weight_hh_z[i][j] * state[j];
-                }
-                // Apply sigmoid activation function
-                z_gate[i] = sigmoid(z_gate[i]);
-            }
-
-            // Compute n gate
-            for (unsigned int i = 0; i < HIDDEN_SIZE; ++i)
-            {
-                // Initialise with bias_n
-                n_gate[i] = bias_ih_n[i];
-                // Matrix-vector multiply with n weights and input
-                for (unsigned int j = 0; j < INPUT_SIZE; ++j) {
-                    n_gate[i] += weight_ih_n[i][j] * input[n][j];
-                }
-                // Initialise with bias_hh
-                n_hidden[i] = bias_hh_n[i];
-                // Matrix-vector multiply with n weights and state
-                for (unsigned int j = 0; j < HIDDEN_SIZE; ++j) {
-                    n_hidden[i] += weight_hh_n[i][j] * state[j];
-                }
-                // Multiply n_hidden by r gate and add it to n gate
-                n_hidden[i] *= r_gate[i];
-                n_gate[i] += n_hidden[i];
-                // Apply tanh activation
-                n_gate[i] = std::tanhf(n_gate[i]);
-            }
-
-            // Compute hidden state candidate
-            for (unsigned int i = 0; i < HIDDEN_SIZE; ++i)
-            {
-                state[i] = (1.f - z_gate[i]) * n_gate[i] + z_gate[i] * state[i];
-            }
-
-            // Affine output
             memcpy(output[n], bias_output, sizeof(bias_output));
-            for (unsigned int i = 0; i < OUTPUT_SIZE; ++i)
-            {
-                for (unsigned int j = 0; j < HIDDEN_SIZE; ++j)
-                {
-                    output[n][i] += weight_output[i][j] * state[j];
+            for (size_t i = 0; i < OUTPUT_SIZE; ++i) {
+                for (size_t j = 0; j < HIDDEN_SIZE; ++j) {
+                    output[n][i] = std::fma(weight_output[i][j], new_state[j], output[n][i]);
                 }
             }
+
+            std::swap(new_state, old_state);
         }
     }
 
@@ -136,17 +84,16 @@ public:
         memcpy(weight_ih_z, params.weight_ih_z, sizeof(weight_ih_z));
         memcpy(weight_ih_n, params.weight_ih_n, sizeof(weight_ih_n));
 
-        memcpy(bias_ih_r, params.bias_ih_r, sizeof(bias_ih_r));
-        memcpy(bias_ih_z, params.bias_ih_z, sizeof(bias_ih_z));
         memcpy(bias_ih_n, params.bias_ih_n, sizeof(bias_ih_n));
+        memcpy(bias_hh_n, params.bias_hh_n, sizeof(bias_hh_n));
+        for (size_t i = 0; i < HIDDEN_SIZE; ++i) {
+            bias_r[i] = params.bias_ih_r[i] + params.bias_hh_r[i];
+            bias_z[i] = params.bias_ih_z[i] + params.bias_hh_z[i];
+        }
 
         memcpy(weight_hh_r, params.weight_hh_r, sizeof(weight_hh_r));
         memcpy(weight_hh_z, params.weight_hh_z, sizeof(weight_hh_z));
         memcpy(weight_hh_n, params.weight_hh_n, sizeof(weight_hh_n));
-
-        memcpy(bias_hh_r, params.bias_hh_r, sizeof(bias_hh_r));
-        memcpy(bias_hh_z, params.bias_hh_z, sizeof(bias_hh_z));
-        memcpy(bias_hh_n, params.bias_hh_n, sizeof(bias_hh_n));
 
         memcpy(weight_output, params.weight_output, sizeof(weight_output));
         memcpy(bias_output, params.bias_output, sizeof(bias_output));
@@ -154,7 +101,8 @@ public:
 
     void reset_state()
     {
-        memset(state, 0, sizeof(state));
+        memset(new_state, 0, sizeof(new_state));
+        memset(old_state, 0, sizeof(old_state));
     }
 
 private:
@@ -162,22 +110,19 @@ private:
     float weight_ih_r[HIDDEN_SIZE][INPUT_SIZE];
     float weight_ih_z[HIDDEN_SIZE][INPUT_SIZE];
     float weight_ih_n[HIDDEN_SIZE][INPUT_SIZE];
-
-    float bias_ih_r[HIDDEN_SIZE];
-    float bias_ih_z[HIDDEN_SIZE];
     float bias_ih_n[HIDDEN_SIZE];
 
     float weight_hh_r[HIDDEN_SIZE][HIDDEN_SIZE];
     float weight_hh_z[HIDDEN_SIZE][HIDDEN_SIZE];
     float weight_hh_n[HIDDEN_SIZE][HIDDEN_SIZE];
-
-    float bias_hh_r[HIDDEN_SIZE];
-    float bias_hh_z[HIDDEN_SIZE];
     float bias_hh_n[HIDDEN_SIZE];
+    float bias_r[HIDDEN_SIZE];
+    float bias_z[HIDDEN_SIZE];
 
     float weight_output[OUTPUT_SIZE][HIDDEN_SIZE];
     float bias_output[OUTPUT_SIZE];
 
     // gru state
-    float state[HIDDEN_SIZE];
+    float new_state[HIDDEN_SIZE];
+    float old_state[HIDDEN_SIZE];
 };
